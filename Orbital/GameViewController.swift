a602a0e9100c91bb0741e069ca715d9644a7971f
Ethan Jackwitz <ethanjackwitz@gmail.com>
2017-06-11 12:44:14 +1000

    Initial commit

--- Orbital/GameViewController.swift
+++ Orbital/GameViewController.swift
@@ -9,71 +9,123 @@
 import UIKit
 import QuartzCore
 import SceneKit
+import ARKit
 
 class GameViewController: UIViewController {
 
+    @IBOutlet var sceneView: ARSCNView!
+
+    let starNode: SCNNode = {
+
+        let geometry = SCNSphere(radius: 0.3)
+
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+        material.locksAmbientWithDiffuse = true
+        //            material.emission.contents = UIColor.red
+        material.selfIllumination.contents = UIColor.red
+        geometry.firstMaterial = material
+
+
+        let node = SCNNode(geometry: geometry)
+
+        node.position = SCNVector3(0, -0.5, -1)
+
+        node.light = SCNLight()
+        node.light!.type = .omni
+
+        return node
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // create and add a camera to the scene
-        let cameraNode = SCNNode()
-        cameraNode.camera = SCNCamera()
-        scene.rootNode.addChildNode(cameraNode)
-        
-        // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
-        
-        // create and add a light to the scene
-        let lightNode = SCNNode()
-        lightNode.light = SCNLight()
-        lightNode.light!.type = .omni
-        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
-        scene.rootNode.addChildNode(lightNode)
-        
-        // create and add an ambient light to the scene
-        let ambientLightNode = SCNNode()
-        ambientLightNode.light = SCNLight()
-        ambientLightNode.light!.type = .ambient
-        ambientLightNode.light!.color = UIColor.darkGray
-        scene.rootNode.addChildNode(ambientLightNode)
-        
-        // retrieve the ship node
-        let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
-        
-        // animate the 3d object
-        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
-        
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
+
+        sceneView.delegate = self
+
+        let planetNode: SCNNode = {
+
+            let geometry = SCNSphere(radius: 0.05)
+
+            let material = SCNMaterial()
+            material.diffuse.contents = #imageLiteral(resourceName: "diffuse")
+            material.ambient.contents = nil
+            material.specular.contents = #imageLiteral(resourceName: "specular")
+            material.emission.contents = nil
+            material.transparent.contents = nil
+            material.reflective.contents = nil
+            material.multiply.contents = nil
+//            material.normal.contents = #imageLiteral(resourceName: "normal")
+
+            geometry.firstMaterial = material
+
+            let node = SCNNode(geometry: geometry)
+
+            let cloudNode: SCNNode = {
+
+                let geometry = SCNSphere(radius: 0.051)
+
+                geometry.firstMaterial!.transparent.contents = #imageLiteral(resourceName: "clouds")
+
+                return SCNNode(geometry: geometry)
+            }()
+
+            node.position = SCNVector3(0.5, -0.5, -1)
+
+            node.addChildNode(cloudNode)
+
+
+            return node
+        }()
+
+        let scene = SCNScene()
+
+        scene.rootNode.addChildNode(starNode)
+        scene.rootNode.addChildNode(planetNode)
+
+        planetNode.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 0.2, z: 0.05, duration: 1)))
+
         // set the scene to the view
-        scnView.scene = scene
-        
-        // allows the user to manipulate the camera
-        scnView.allowsCameraControl = true
+        sceneView.scene = scene
         
         // show statistics such as fps and timing information
-        scnView.showsStatistics = true
+        sceneView.showsStatistics = true
         
         // configure the view
-        scnView.backgroundColor = UIColor.black
+        sceneView.backgroundColor = UIColor.black
         
         // add a tap gesture recognizer
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-        scnView.addGestureRecognizer(tapGesture)
+        sceneView.addGestureRecognizer(tapGesture)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        let configuration = ARWorldTrackingSessionConfiguration()
+        configuration.planeDetection = .horizontal
+
+
+        sceneView.session.delegate = self
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        // Pause the view's session
+        sceneView.session.pause()
     }
     
     @objc
     func handleTap(_ gestureRecognize: UIGestureRecognizer) {
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
+
+        guard let currentFrame = sceneView.session.currentFrame else {
+            return
+        }
         // check what nodes are tapped
-        let p = gestureRecognize.location(in: scnView)
-        let hitResults = scnView.hitTest(p, options: [:])
+        let p = gestureRecognize.location(in: sceneView)
+        let hitResults = sceneView.hitTest(p, options: [:])
         // check that we clicked on at least one object
         if hitResults.count > 0 {
             // retrieved the first clicked object
@@ -81,6 +133,8 @@ class GameViewController: UIViewController {
             
             // get its material
             let material = result.node.geometry!.firstMaterial!
+
+            print(result.node.position)
             
             // highlight it
             SCNTransaction.begin()
@@ -99,6 +153,18 @@ class GameViewController: UIViewController {
             material.emission.contents = UIColor.red
             
             SCNTransaction.commit()
+        } else {
+
+            let imagePlane = SCNPlane(width: sceneView.bounds.width / 6000, height: sceneView.bounds.height / 6000)
+            imagePlane.firstMaterial?.diffuse.contents = sceneView.snapshot()
+            imagePlane.firstMaterial?.lightingModel = .constant
+
+            let planeNode = SCNNode(geometry: imagePlane)
+            sceneView.scene.rootNode.addChildNode(planeNode)
+
+            var translation = matrix_identity_float4x4
+            translation.columns.3.z = -0.1
+            planeNode.simdTransform = matrix_multiply(currentFrame.camera.transform, translation)
         }
     }
     
@@ -122,5 +188,14 @@ class GameViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
+}
 
+extension GameViewController: ARSCNViewDelegate, ARSessionDelegate {
+
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        sceneView.backgroundColor = nil
+//        if let lightEstimate = frame.lightEstimate {
+//            self.starNode.light?.intensity = lightEstimate.ambientIntensity
+//        }
+    }
 }
